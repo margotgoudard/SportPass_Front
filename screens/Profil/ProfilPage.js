63102349dc42f5e612ee0c5ce40bc899e810beb7
margotgoudard <goudard.margot@hotmail.com>
2024-04-23 10:41:05 +0200

    profilPage

--- screens/Profil/ProfilPage.js
+++ screens/Profil/ProfilPage.js
@@ -2,28 +2,27 @@ import React, { useState, useEffect, useCallback } from 'react';
 import {
   View,
   Text,
-  Button,
   TouchableOpacity,
   StyleSheet,
   Image,
   ScrollView,
   ImageBackground,
-  Alert
+  Alert,
+  ActivityIndicator
 } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import moment from 'moment';
 import 'moment/locale/fr';
-import PostComponent from '../../components/PostComponent.js';
+import PostComponent from '../components/PostComponent.js';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import { ActivityIndicator } from 'react-native';
-import EditModal from '../../components/EditModal.js';
-import MessageModal from '../../components/MessageModal.js';
+import EditActions from '../components/EditActionsComponent.js';
+import MessageModal from '../components/MessageModal.js';
 
 moment.locale('fr');
 
-const UserProfilePage = ({ route }) => {
+const ProfilePage = ({ route }) => {
   const { userData } = route.params;
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
@@ -35,10 +34,12 @@ const UserProfilePage = ({ route }) => {
   const [messageText, setMessageText] = useState('');
   const [inputHeight, setInputHeight] = useState(0); 
   const [messageModalVisible, setMessageModalVisible] = useState(false);
+  const [blurEffect, setBlurEffect] = useState(false);
+  const [postComponentVisible, setPostComponentVisible] = useState(false);
 
   const adjustInputHeight = (event) => {
     setInputHeight(event.nativeEvent.contentSize.height);
-};
+  };
 
   const calculateProfileCompletion = (user) => {
     const fields = [
@@ -53,11 +54,6 @@ const UserProfilePage = ({ route }) => {
     ];
     const filledFields = fields.filter(Boolean);
     return (filledFields.length / fields.length) * 100;
-  };
-
-  const handleLongPressOnPost = (post) => {
-    setSelectedPost(post);
-    setEditModalVisible(true);
   };
 
   const modifyPost = async (idPublication, newContent) => {
@@ -75,20 +71,101 @@ const UserProfilePage = ({ route }) => {
     }
   };
 
-const deletePost = async (idPublication) => {
+  const deletePost = async (idPublication) => {
     try {
-        const response = await axios.delete(`http://10.0.2.2:4000/api/publicationUser/${idPublication}`);
-        fetchUserPosts();
-      } catch (error) {
-        console.error('Error delete post', error);
-        return false; 
-      }
-    };
+      await axios.delete(`http://10.0.2.2:4000/api/publicationUser/${idPublication}`);
+      fetchUserPosts();
+    } catch (error) {
+      console.error('Error delete post', error);
+      return false; 
+    }
+  };
 
+  const handleLongPressOnPost = (post) => {
+    setSelectedPost(post);
+    setEditModalVisible(true);
+    setPostComponentVisible(true);
+    setBlurEffect(true);
+  };
+
+  const disableBlurAndEditModal = () => {
+    setBlurEffect(false);
+    setEditModalVisible(false);
+    setPostComponentVisible(true);
+    setSelectedPost(null);
+    renderSelectedPost();
+  };
+
+  const renderBlurOverlay = () => (
+    <TouchableOpacity
+      style={[styles.blurOverlay, { display: blurEffect ? 'flex' : 'none' }]}
+      onPress={disableBlurAndEditModal} 
+      activeOpacity={1}
+    />
+  );
+
+  const renderSelectedPost = () => {
+    if (!selectedPost || !blurEffect) return null;
+    console.log(selectedPost, blurEffect);
+    return (
+      <View>
+        <View style={styles.selectedPostContainer}>
+          {postComponentVisible && (
+            <PostComponent
+              post={selectedPost}
+              onPostPress={() => {}}
+              onLongPress={() => {}}
+            />
+          )}
+          {editModalVisible && (
+            <EditActions
+              onClose={() => {
+                setEditModalVisible(false);
+                setPostComponentVisible(false);
+              }}
+              onEdit={() => {
+                setEditModalVisible(false);
+                setPostComponentVisible(false); 
+                setMessageText(selectedPost.contenu);
+                setMessageModalVisible(true);
+              }}
+              onDelete={() => {
+                setEditModalVisible(false);
+                setPostComponentVisible(false);
+                deletePost(selectedPost.idPublication);
+              }}
+            />
+          )}
+        </View>
+        <MessageModal
+          isModalVisible={messageModalVisible}
+          closeModal={() => {
+            setMessageModalVisible(false);
+            setEditModalVisible(false); 
+            setPostComponentVisible(false);
+            setBlurEffect(false); 
+            setSelectedPost(null);
+          }}
+          messageText={messageText}
+          setMessageText={setMessageText}
+          sendMessage={() => {
+            modifyPost(selectedPost.idPublication, messageText);
+            setMessageModalVisible(false);
+            setPostComponentVisible(false);
+            setEditModalVisible(false); 
+            setBlurEffect(false);
+            setSelectedPost(null); 
+          }}
+          adjustInputHeight={adjustInputHeight}
+          inputHeight={inputHeight}
+        />
+      </View>
+    );
+  };
 
   const handleLogOut = async () => {
     try {
-      await AsyncStorage.removeItem('userToken');
+      await AsyncStorage.removeItem('@userToken');
       navigation.navigate('Login');
     } catch (error) {
       console.error('Log out failed', error);
@@ -139,20 +216,22 @@ const deletePost = async (idPublication) => {
 
   if (!user) {
     return (
-        <ImageBackground source={require('../../assets/background.png')} style={styles.container}>
-            <View style={styles.loadingContainer}>
-                <ActivityIndicator size="large" color="#008900" />
-            </View>
-        </ImageBackground>
+      <ImageBackground source={require('../assets/background.png')} style={styles.container}>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      </ImageBackground>
     );
-}
+  }
 
   const navigateToModifyProfile = () => {
     navigation.navigate('ModificationProfil', { user });
   };
 
   return (
-    <ImageBackground source={require('../../assets/background.png')} style={styles.container}>
+    <ImageBackground source={require('../assets/background.png')} style={styles.container}>
+      {renderBlurOverlay()}
+      {renderSelectedPost()} 
       <ScrollView>
         <View style={styles.topBar}>
           <TouchableOpacity onPress={handleLogOut}>
@@ -163,13 +242,13 @@ const deletePost = async (idPublication) => {
           </TouchableOpacity>
         </View>
         <View style={styles.headerContainer}>
-          <Image source={require('../../assets/avatar.png')} style={styles.avatar} />
+          <Image source={require('../assets/avatar.png')} style={styles.avatar} />
           <View style={styles.userInfoContainer}>
             <View style={styles.vipStatusContainer}>
               <Text style={styles.vipStatus}>
                 Palier {user.Palier?.nom}
               </Text>
-              <Image source={require('../../assets/palier.png')} style={styles.palierImage} />
+              <Image source={require('../assets/palier.png')} style={styles.palierImage} />
             </View>
             <Text style={styles.bold}>{user.pseudo}</Text>
             <Text style={styles.teamName}>{user.Equipe?.nom}</Text>
@@ -195,76 +274,48 @@ const deletePost = async (idPublication) => {
                 <Text style={{ marginLeft: 5 }}>{user.adresse || "Veuillez saisir votre adresse..."}</Text>
               </View>
             </Text>
-            <View style={styles.progressBarContainer}>
-              <View style={[styles.progressBar, { width: `${calculateProfileCompletion(user)}%` }]} />
-            </View>
-            <TouchableOpacity onPress={navigateToModifyProfile}>
-              <Text style={[styles.progressBarText, calculateProfileCompletion(user) === 100 ? styles.profileCompleted : {}]}>
-                {`${Math.round(calculateProfileCompletion(user))}% de votre profil est complet`}
-              </Text>
-            </TouchableOpacity>
+            {calculateProfileCompletion(user) < 100 && (
+              <>
+                <View style={styles.progressBarContainer}>
+                  <View style={[styles.progressBar, { width: `${calculateProfileCompletion(user)}%` }]} />
+                </View>
+                <Text style={[styles.progressBarText, calculateProfileCompletion(user) === 100 ? styles.profileCompleted : {}]}>
+                  {`${Math.round(calculateProfileCompletion(user))}% de votre profil est complet`}
+                </Text>
+              </>
+            )}
             <View style={styles.listContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('Pass', { userId: userData.idUser })}>
-
-            <View style={styles.listItem}>
-                <Text style={styles.listItemText}>Mon abonnement</Text>
-              
-              <Text style={styles.listItemArrow}>›</Text>
-            </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Billet', { userId: userData.idUser })}>
-
-              <View style={styles.listItem}>
-                <Text style={styles.listItemText}>Mes billets</Text>
-                <Text style={styles.listItemArrow}>›</Text>
-              </View>
+              <TouchableOpacity onPress={() => navigation.navigate('Pass', { userId: userData.idUser })}>
+                <View style={styles.listItem}>
+                  <Text style={styles.listItemText}>Mon abonnement</Text>
+                  <Text style={styles.listItemArrow}>›</Text>
+                </View>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate('CommercantFavoris', { userId: userData.idUser })}>
-
-              <View style={styles.listItem}>
-                <Text style={styles.listItemText}>Mes favoris</Text>
-                <Text style={styles.listItemArrow}>›</Text>
-              </View>
+              <TouchableOpacity onPress={() => navigation.navigate('Billet', { userId: userData.idUser })}>
+                <View style={styles.listItem}>
+                  <Text style={styles.listItemText}>Mes billets</Text>
+                  <Text style={styles.listItemArrow}>›</Text>
+                </View>
               </TouchableOpacity>
-
+              <TouchableOpacity onPress={() => navigation.navigate('Commercant', { userId: userData.idUser })}>
+                <View style={styles.listItem}>
+                  <Text style={styles.listItemText}>Mes favoris</Text>
+                  <Text style={styles.listItemArrow}>›</Text>
+                </View>
+              </TouchableOpacity>
+              <View style={styles.postItemList}>
+                {posts.map((post, index) => (
+                  <PostComponent
+                    key={index}
+                    post={post}
+                    onPostPress={() => navigation.navigate('PostDetails', { post })}
+                    onLongPress={() => handleLongPressOnPost(post)}
+                    style={styles.postComponentStyle}
+                  />
+                ))}
+              </View>
             </View>
           </View>
-          <View style={styles.postItemList}>
-          {posts.map((post, index) => (
-        <PostComponent
-          key={index}
-          post={post}
-          onPostPress={() => navigation.navigate('PostDetails', { post })}
-          onLongPress={() => handleLongPressOnPost(post)}
-        />
-      ))}
-      <EditModal
-        isVisible={editModalVisible}
-        onClose={() => setEditModalVisible(false)}
-        onEdit={() => {
-          setEditModalVisible(false); 
-          setMessageText(selectedPost.contenu);
-          setMessageModalVisible(true);
-          modifyPost(selectedPost.idPublication, selectedPost.contenu );
-        }}
-        onDelete={() => {
-          setEditModalVisible(false);
-          deletePost(selectedPost.idPublication);
-        }}
-      />
-      <MessageModal
-          isModalVisible={messageModalVisible}
-          closeModal={() => setMessageModalVisible(false)}
-          messageText={messageText}
-          setMessageText={setMessageText}
-          sendMessage={() => {
-              modifyPost(selectedPost.idPublication, messageText);
-              setMessageModalVisible(false);
-          }}
-          adjustInputHeight={adjustInputHeight}
-          inputHeight={inputHeight}
-      />
-    </View>
         </View>
       </ScrollView>
     </ImageBackground>
@@ -272,144 +323,159 @@ const deletePost = async (idPublication) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    headerContainer: {
-        alignItems: 'center',
-        padding: 20,
-        paddingTop: 50, 
-    },
-    avatar: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-        position: 'absolute', 
-        top: 70, 
-        alignSelf: 'center', 
-        zIndex: 1, 
-    },
-    userInfoContainer: {
-        width: '112%', 
-        backgroundColor: '#D9D9D9',
-        borderRadius: 20,
-        padding: 20,
-        paddingTop: 60,
-        marginTop: 70,
-        position: 'relative',
-    },
-    vipStatusContainer: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        borderRadius: 20,
-        flexDirection: 'row',
-    },
-    vipStatus: {
-        color: 'green',
-        fontWeight: 'bold',
-        fontSize: 15
-    },
-    details: {
-        fontSize: 16,
-        marginBottom: "4%",
-
-    },
-    teamName: {
-        fontSize: 16,
-        marginBottom: 5,
-        color: 'green', 
-        fontWeight: 'bold',
-        textAlign: 'center'
-    },
-    center: {
-        fontSize: 16,
-        marginBottom: "5%",
-        textAlign: 'center'
-    },
-    bold: {
-        fontSize: 16,
-        marginBottom: 5,
-        textAlign: 'center',
-        fontWeight: 'bold'
-    },
-    progressBarContainer: {
-        height: 10,
-        width: '100%',
-        backgroundColor: 'black',
-        borderRadius: 10,
-        marginTop: 10,
-    },
-    progressBar: {
-        height: '100%',
-        backgroundColor: 'green',
-        borderRadius: 10,
-    },
-    progressBarText: {
-        textAlign: 'center',
-        marginTop: 5,
-    },
-    listContainer: {
-        marginTop: 20,
-    },
-    listItem: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        marginBottom: 10
-    },
-    listItemText: {
-        fontSize: 16,
-    },
-    listItemArrow: {
-        fontSize: 16,
-        color: 'grey', 
-    },
-    palierImage: {
-        width: 20, 
-        height: 20, 
-        marginLeft: 5, 
-    },
-    boldNumbers: {
-        fontWeight: 'bold', 
-    },
-    postItemList: {
-        width: '111%'
-    },
-    topBar: {
-        position: 'absolute',
-        top: 40,
-        left: 20,
-        right: 20, 
-        flexDirection: 'row',
-        justifyContent: 'space-between', 
-        alignItems: 'center', 
-    },
-    logoutText: {
-        color: '#BD4F6C',
-        fontWeight: 'bold',
-        fontSize: 16
-    },
-    editProfileText: {
-        color: 'green',
-        fontWeight: 'bold',
-        fontSize: 16,
-    },
-    profileCompleted: {
-        textDecorationLine: 'underline',
-        color: 'green',
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },    
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 50, 
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    position: 'absolute', 
+    top: 70, 
+    alignSelf: 'center', 
+    zIndex: 1, 
+  },
+  userInfoContainer: {
+    width: '112%', 
+    backgroundColor: '#D9D9D9',
+    borderRadius: 20,
+    padding: 20,
+    paddingTop: 60,
+    marginTop: 70,
+    position: 'relative',
+  },
+  vipStatusContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    flexDirection: 'row',
+  },
+  vipStatus: {
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+  details: {
+    fontSize: 16,
+    marginBottom: "4%",
+  },
+  teamName: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: 'green', 
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  center: {
+    fontSize: 16,
+    marginBottom: "5%",
+    textAlign: 'center'
+  },
+  bold: {
+    fontSize: 16,
+    marginBottom: 5,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  progressBarContainer: {
+    height: 10,
+    width: '100%',
+    backgroundColor: 'black',
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  progressBar: {
+    height: '100%',
+    backgroundColor: 'green',
+    borderRadius: 10,
+  },
+  progressBarText: {
+    textAlign: 'center',
+    marginTop: 5,
+  },
+  listContainer: {
+    marginTop: 20,
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+  listItemArrow: {
+    fontSize: 16,
+    color: 'grey', 
+  },
+  palierImage: {
+    width: 20, 
+    height: 20, 
+    marginLeft: 5, 
+  },
+  boldNumbers: {
+    fontWeight: 'bold', 
+  },
+  postItemList: {
+    width: '100%'
+  },
+  topBar: {
+    position: 'absolute',
+    top: 40,
+    left: 20,
+    right: 20, 
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+  },
+  logoutText: {
+    color: '#BD4F6C',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  editProfileText: {
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  profileCompleted: {
+    textDecorationLine: 'underline',
+    color: 'green',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },   
+  blurOverlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 1, 
+  },
+  selectedPostContainer: {
+    position: 'absolute',
+    zIndex: 2, 
+    width: '90%',
+    alignSelf: "center",
+    marginTop: "70%"
+  }
 });
 
-export default UserProfilePage;
+export default ProfilePage;
