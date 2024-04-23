58402f4e18b22cbc322b672931238b0b309ff591
margotgoudard <goudard.margot@hotmail.com>
2024-04-23 10:29:18 +0200

    messageModal

--- components/MessageModal.js
+++ components/MessageModal.js
@@ -51,13 +51,6 @@ const styles = StyleSheet.create({
         padding: 35,
         alignItems: "center",
         shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
         width: '80%', 
         height: '40%', 
     },
