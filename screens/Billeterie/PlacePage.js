cde4b7b22b76f006ec7ca319d3e561679d5160d9
inesamzr <ines.amzert@etu.umontpellier.fr>
2024-04-30 15:12:20 +0200

    affichage et sélection de places sans le style

--- screens/Billeterie/PlacePage.js
+++ screens/Billeterie/PlacePage.js
@@ -1,94 +1,153 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, ImageBackground } from 'react-native';
 import LottieView from 'lottie-react-native';
 import AppLoader from '../../components/AppLoader';
 import axios from 'axios'; 
 import ProgressBar from '../../components/ProgressBar';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function PlacePage({ route }) {
+  const { selectedTribune } = route.params;
+  const [selectedPlaces, setSelectedPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [placesByRow, setPlacesByRow] = useState([]);
+  const [tickets, setTickets] = useState([]);
 
 
-export default function PlacePage({route}){
-const { selectedTribune } = route.params;
-const [selectedPlace, setSelectedPlace] = useState(null);
-const [loading, setLoading] = useState(true);
-const [places, setPlaces] = useState([]);
-const [tickets, setTickets] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const rangeeResponse = await axios.get(`http://10.0.2.2:4000/api/rangee/tribune/${selectedTribune.idTribune}`);
+        const rangees = rangeeResponse.data;
 
+        const placesResponse = await Promise.all(
+          rangees.map((rangee) => axios.get(`http://10.0.2.2:4000/api/place/rangee/${rangee.idRangee}`))
+        );
+        const placesData = placesResponse.map((response) => response.data).flat();
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const rangeeResponse = await axios.get(`http://10.0.2.2:4000/api/rangee/tribune/${selectedTribune.idTribune}`);
-      const rangees = rangeeResponse.data;
-      //console.log(rangees);
+        const placesByRowData = {};
+        placesData.forEach((place) => {
+          if (!placesByRowData[place.idRangee]) {
+            placesByRowData[place.idRangee] = [];
+          }
+          placesByRowData[place.idRangee].push(place);
+        });
 
-      const placesResponse = await Promise.all(
-        rangees.map((rangee) => axios.get(`http://10.0.2.2:4000/api/place/rangee/${rangee.idRangee}`))
-      );
-      const placesData = placesResponse.map((response) => response.data);
-      //console.log(placesData);
+        setPlacesByRow(placesByRowData);
 
-      const allPlaces = placesData.flat();
-      //console.log(allPlaces);
-      setPlaces(allPlaces);
+        const ticketResponse = await Promise.all(
+          placesData.map((place)=> axios.get(`http://10.0.2.2:4000/api/billet/place/${place.idPlace}`))
+        );
+        const ticketData = ticketResponse.map((response) => response.data);
+        console.log(ticketData);
+  
+        setTickets(ticketData);
 
-      const ticketResponse = await Promise.all(
-        allPlaces.map((place)=> axios.get(`http://10.0.2.2:4000/api/billet/place/${place.idPlace}`))
-      );
-      const ticketData = ticketResponse.map((response) => response.data);
-      console.log(ticketData);
-
-      setTickets(ticketData);
-
-      setLoading(false);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des places :', error);
-      setLoading(false);
-    }
-  };
-  fetchData();
+        setLoading(false);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des places :', error);
+        setLoading(false);
+      }
+    };
+    fetchData();
   }, [selectedTribune]);
 
+   const getreservee = (billet) => {
+    return billet.reservee ? 'green' : 'red';
+  };
+
+  const getTicketByPlaceId = (placeId) => {
+    return tickets.find((ticket) => ticket.idPlace === placeId);
+  };
+
+  const handlePlaceSelection = (place) => {
+    const isSelected = selectedPlaces.some((selectedPlace) => selectedPlace.idPlace === place.idPlace);
+    if (isSelected) {
+      const filteredPlaces = selectedPlaces.filter((selectedPlace) => selectedPlace.idPlace !== place.idPlace);
+      setSelectedPlaces(filteredPlaces);
+    } else {
+      setSelectedPlaces([...selectedPlaces, place]);
+    }
+  };
 
   if (loading) {
-    return (
-        <AppLoader/>
-    );
+    return <AppLoader />;
   }
 
   return (
     <ImageBackground source={require('../../assets/background.png')} style={styles.background}>
       <ScrollView>
         <ProgressBar currentPage={3} />
-      {places.map((place,index) => (
-        <TouchableOpacity
-          key={index}
-          style={[styles.place, selectedPlace === index && styles.selectedPlace]}
-          onPress={() => setSelectedPlace(place)}
-        >
-          <Text>Rang {place.idRangee} - Siège {place.numero}</Text>
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
+        <View style={styles.container}>
+        {Object.keys(placesByRow).map((rowId) => (
+          <View key={rowId}>
+            <View style={styles.rowContainer}>
+              {placesByRow[rowId].map((place, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={[
+                    styles.place, 
+                    selectedPlaces.some((selectedPlace) => selectedPlace.idPlace === place.idPlace) && styles.selectedPlace
+                  ]}
+                    onPress={() => handlePlaceSelection(place)}
+                >
+                   {(getTicketByPlaceId(place.idPlace) && getTicketByPlaceId(place.idPlace).reservee) ? (
+                      <MaterialIcons 
+                        name="chair" 
+                        size={24} 
+                        color='green'
+                      />
+                    ) : (
+                      <MaterialIcons 
+                        name="chair" 
+                        size={24} 
+                        color='red'
+                      />
+                    )}
+                  
+                </TouchableOpacity>
+              ))}
+            </View>
+            
+          </View>
+          
+        ))}
+        </View>
+      </ScrollView>
     </ImageBackground>
   );
-  };
+}
 
-  const styles = StyleSheet.create({
-    background: {
-      flex: 1,
-      resizeMode: 'cover',
-      justifyContent: 'center',
-    },
-    container: {
-      padding: 20,
-    },
-    place: {
-      padding: 10,
-      marginVertical: 5,
-      backgroundColor: 'lightgrey',
-      borderRadius: 5,
-    },
-    selectedPlace: {
-      backgroundColor: 'blue',
-    },
-  });
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  container:{
+    marginBottom:60,
+    marginTop:20,
+
+  },
+  rowHeader: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent:'center',
+  },
+  place: {
+    margin: 5,
+    backgroundColor: 'lightgrey',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectedPlace: {
+    backgroundColor: 'blue',
+  },
+});
