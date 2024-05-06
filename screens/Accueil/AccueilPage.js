5322ac62158c4f82253b47b1fd02479a2876b8a8
inesamzr <ines.amzert@etu.umontpellier.fr>
2024-05-06 13:13:04 +0200

    ajout du bandeau derrière les publications

--- screens/Accueil/AccueilPage.js
+++ screens/Accueil/AccueilPage.js
@@ -7,13 +7,11 @@ export default function Accueil({ navigation }) {
     const [userFirstName, setUserFirstName] = useState(null);
     const [alaUnePublications, setAlaUnePublications] = useState([]);
 
-
     useEffect(() => {
         const fetchUserData = async () => {
             try {
                 const userId = await AsyncStorage.getItem('userId');
                 const response = await axios.get(`http://10.0.2.2:4000/api/user/${userId}`);
-
                 const user = response.data;
                 setUserFirstName(user.prenom);
             } catch (error) {
@@ -25,7 +23,6 @@ export default function Accueil({ navigation }) {
             try {
                 const userId = await AsyncStorage.getItem('userId');
                 const response = await axios.get(`http://10.0.2.2:4000/api/user/${userId}`);
-                
                 const user = response.data;
                 const idEquipe = user.idEquipe; 
 
@@ -44,35 +41,34 @@ export default function Accueil({ navigation }) {
     return (
         <ImageBackground source={require('../../assets/background.png')} style={styles.background}>
             <ScrollView>
-
-            <View style={styles.logoContainer}>
-                <Image source={require('../../assets/logo.png')} style={styles.logo} />
-            </View>
-            <View style={styles.container}>
-                {userFirstName && (
-                    <Text style={styles.bienvenue}>
-                        Bonjour, {userFirstName} !
-                    </Text>
-                )}
-
-                {alaUnePublications.length > 0 && (
-                    <View style={styles.publicationsContainer}>
-                        <Text style={styles.title}>À la Une</Text>
-                        <ScrollView horizontal={true}>
-                        {alaUnePublications.map((publication, index) => (
-                                <View key={index}>
-                                    <Image 
-                                        source={{ uri: publication.image }}
-                                        style={styles.publicationImage} 
-                                    />
-                                </View>
-                        ))} 
-                        </ScrollView>
-
-                    </View>
-                )}
-
-            </View>
+                <View style={styles.logoContainer}>
+                    <Image source={require('../../assets/logo.png')} style={styles.logo} />
+                </View>
+                <View style={styles.container}>
+                    {userFirstName && (
+                        <Text style={styles.bienvenue}>
+                            Bonjour, {userFirstName} !
+                        </Text>
+                    )}
+                    {alaUnePublications.length > 0 && (
+                        <View style={styles.uneContainer}>
+                            <View style={styles.publicationsContainer}>
+                                <Text style={styles.title}>À la Une</Text>
+                                <ScrollView horizontal={true}>
+                                    {alaUnePublications.map((publication, index) => (
+                                        <View key={index} style={styles.imageContainer}>
+                                            <Image 
+                                                source={{ uri: publication.image }}
+                                                style={styles.publicationImage} 
+                                            />
+                                        </View>
+                                    ))} 
+                                </ScrollView>
+                            </View>
+                            <View style={styles.bandeau}></View>
+                        </View>
+                    )}
+                </View>
             </ScrollView>
         </ImageBackground>
     );
@@ -87,30 +83,47 @@ const styles = StyleSheet.create({
     logoContainer: {
         justifyContent: 'flex-start', 
         alignItems: 'flex-start',
-        marginTop:20, 
-        marginLeft:15,       
-      },
-   
+        marginTop: 20, 
+        marginLeft: 15,       
+    },
     container: {
         flex: 1,
     },
     bienvenue: {
         fontSize: 20,
-        position:'absolute',
-        marginLeft:20,
+        position: 'absolute',
+        marginLeft: 20,
         fontWeight: 'bold',
     },
     publicationsContainer: {
         marginTop: 30,
+        zIndex: 1, 
     },
     title: {
         fontSize: 18,
-        fontWeight: 'bold',
         marginBottom: 10,
+        marginLeft: 15,
+    },
+    imageContainer: {
+        marginLeft: 15,
+        borderRadius: 10,
+        overflow: 'hidden', 
+        position: 'relative', 
     },
     publicationImage: {
-        width: 100,
-        height: 100,
-        marginRight : 10,
+        width: 342,
+        height: 207,
+    },
+    bandeau: {
+        backgroundColor: '#D9D9D9',
+        height: 100, 
+        position: 'absolute',
+        bottom: 50, 
+        left: 0,
+        right: 0,
+        zIndex: 0, 
+    },
+    uneContainer: {
+        position: 'relative',
     },
 });
